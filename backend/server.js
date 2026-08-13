ac3a4308296c55a1270ba240d614486dc166461e
kai <kaibeyblade777@gmail.com>
2026-08-13 13:15:17 +0530

    #11 | PJ | establish the database conncetion and implemented the nodemon server for restarting

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/connectToDb')
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const app = express()
 const PORT = process.env.PORT
+
+connectDB()
 
 app.use(cors())
 app.use(express.json())
