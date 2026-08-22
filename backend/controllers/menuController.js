0e502e2b43a5c532b5055ef623cd49e9fd7b30dc
jagadeeshpj1610 <22022cm040@gmail.com>
2026-08-22 13:19:22 +0530

    #12 | PJ | add the delete menu item route and its logic controller

--- backend/controllers/menuController.js
+++ backend/controllers/menuController.js
@@ -11,24 +11,38 @@ const getMenuItems = async (req, res) => {
 
 const createMenuItem = async (req, res) => {
     try {
-        const {name, price, category, description, isAvailable } = req.body;
-        const newItem = await Menu.create({name, price, category, description, isAvailable})
+        const { name, price, category, description, isAvailable } = req.body;
+        const newItem = await Menu.create({ name, price, category, description, isAvailable })
         res.status(201).json(newItem)
     } catch (error) {
-        res.status(500).json({message : "failed to create menu item"})
+        res.status(500).json({ message: "failed to create menu item" })
     }
 }
 
 const updateMenuItem = async (req, res) => {
     try {
-        const {id} = req.params;
-        const {name, price, category, description, isAvailable } = req.body;
-        const updatedOne = await Menu.findByIdAndUpdate(id, {name, price, category, description, isAvailable}, { returnDocument: 'after' })
+        const { id } = req.params;
+        const { name, price, category, description, isAvailable } = req.body;
+        const updatedOne = await Menu.findByIdAndUpdate(id, { name, price, category, description, isAvailable }, { returnDocument: 'after' })
         console.log("update succesful");
         res.status(200).json(updatedOne)
     } catch (error) {
-        res.status(500).json({message : "failed to update the menu item"})
+        res.status(500).json({ message: "failed to update the menu item" })
     }
 }
 
-module.exports = {getMenuItems, createMenuItem, updateMenuItem}
+const deleteMenuItem = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedItem = await Menu.findByIdAndDelete(id)
+        console.log("deleted succesfully");
+
+        if (!deletedItem) return res.status(404).json({ message: 'Menu item not found' });
+        
+        res.status(200).json({ message: "Menu item deleted Sucessfully" })
+    } catch (error) {
+        res.status(500).json({ message: "failed to delete the menu item" })
+    }
+}
+
+module.exports = { getMenuItems, createMenuItem, updateMenuItem, deleteMenuItem }
--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const router = express.Router();
 
-const {getMenuItems, createMenuItem, updateMenuItem} = require('../controllers/menuController')
+const {getMenuItems, createMenuItem, updateMenuItem, deleteMenuItem} = require('../controllers/menuController')
 
 router.get('/', getMenuItems )
 router.post('/', createMenuItem)
 router.put('/:id', updateMenuItem)
+router.delete('/:id', deleteMenuItem)
 
 module.exports = router
