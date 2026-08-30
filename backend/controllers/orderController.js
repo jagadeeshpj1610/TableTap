f891c0026700d7ed807b8d3da353b2f9179395fd
jagadeeshpj1610 <22022cm040@gmail.com>
2026-08-30 12:21:06 +0530

    #13 | PJ | handle the unknown status instead of order status keywords

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -47,7 +47,7 @@ const updateOrderStatus = async (req, res) => {
     try {
         const {id} = req.params;
         const {status} = req.body;
-        const updatedOrder = await Order.findByIdAndUpdate(id, {status}, {returnDocument : 'after'})
+        const updatedOrder = await Order.findByIdAndUpdate(id, {status}, {returnDocument : 'after', runValidators : true})
         res.status(200).json(updatedOrder)
     } catch (error) {
         res.status(500).json({message : 'failed to update the order status'})
