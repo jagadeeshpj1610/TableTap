96a1de7bd86e0a99de1d6bad6840527248c8a172
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-01 11:58:07 +0530

    #17 #B | PJ | remove the console logs

--- frontend/src/api/menuApi.js
+++ frontend/src/api/menuApi.js
@@ -4,9 +4,7 @@ const defaultApi = "http://localhost:5000/api";
 const getMenu = async () => {
     try {
         const response = await fetch(`${defaultApi}/menu`)
-        console.log(response);
         const fetchedData = await response.json();
-        // console.log(fetchedData);
         return fetchedData;
     } catch (error) {
         console.error("failed to fetch the menu:", error.message);
