0a5977dfa70a232bb35f8485e4a1d67b4b40746b
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-01 08:07:13 +0530

    #16 #A | resolve the waiter call status

--- backend/controllers/waiterCallController.js
+++ backend/controllers/waiterCallController.js
@@ -3,11 +3,11 @@ const WaiterCallSchema = require('../models/WaiterCall')
 
 const createWaiterCall = async (req, res) => {
     try {
-        const {tableNumber} = req.body;
-        const createdCall = await WaiterCallSchema.create({tableNumber})
+        const { tableNumber } = req.body;
+        const createdCall = await WaiterCallSchema.create({ tableNumber })
         res.status(201).json(createdCall)
     } catch (error) {
-        res.status(500).json({message:'failed to created the waiter call'})
+        res.status(500).json({ message: 'failed to created the waiter call' })
     }
 }
 
@@ -18,8 +18,18 @@ const getAllWaiterCalls = async (req, res) => {
         res.status(200).json(allCalls);
     } catch (error) {
         console.log("failed to fetch the waiter calls");
-        res.status(500).json({message : "failed to get the waiter calls"});
+        res.status(500).json({ message: "failed to get the waiter calls" });
     }
 }
 
-module.exports = {createWaiterCall, getAllWaiterCalls}
+const resolveWaiterCall = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const resolvedStatus = await WaiterCallSchema.findByIdAndUpdate(id, { status: "resolved" }, { returnDocument: 'after', runValidators: true })
+        res.status(200).json(resolvedStatus)
+    } catch (error) {
+        res.status(500).json({message : "failed to update the status"})
+    }
+}
+
+module.exports = { createWaiterCall, getAllWaiterCalls, resolveWaiterCall }
--- backend/routes/waiterCallRoutes.js
+++ backend/routes/waiterCallRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router();
 
-const {createWaiterCall, getAllWaiterCalls} = require('../controllers/waiterCallController')
+const {createWaiterCall, getAllWaiterCalls, resolveWaiterCall} = require('../controllers/waiterCallController')
 
 router.post('/call', createWaiterCall);
 router.get('/', getAllWaiterCalls)
+router.patch('/:id/resolve', resolveWaiterCall)
 
 module.exports = router;
