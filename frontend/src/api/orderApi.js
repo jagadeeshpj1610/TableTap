7cc48f952d2defba5df43a87ad5f075e468a5f1d
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-08 17:02:36 +0530

    #20 #B | PJ | get the current order bill

--- frontend/src/api/orderApi.js
+++ frontend/src/api/orderApi.js
@@ -3,20 +3,20 @@ const defaultApi = "http://localhost:5000/api";
 const createOrder = async (tableNumber, orderItems) => {
     try {
         const response = await fetch(`${defaultApi}/orders`, {
-            method:"POST",
-            headers:{"Content-type" : "application/json"},
-            body:JSON.stringify({
-                tableNumber : tableNumber,
-                items : orderItems
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({
+                tableNumber: tableNumber,
+                items: orderItems
             })
         })
         const data = await response.json()
         console.log("order placed with this data :", data);
         return data
-        
+
     } catch (error) {
-        console.error("failed to plcae the order : ",error);
-        
+        console.error("failed to plcae the order : ", error);
+
     }
 }
 
@@ -30,4 +30,15 @@ const getOrderById = async (id) => {
     }
 };
 
-export  { createOrder, getOrderById }
+const getOrderBill = async (id) => {
+    try {
+        const response = await fetch(`${defaultApi}/orders/${id}/bill`)
+        const data = await response.json();
+        console.log("bill is fetched", data);
+        return data
+    } catch (error) {
+        console.error("get the order bill was failed : ", error);
+    }
+}
+
+export { createOrder, getOrderById, getOrderBill }
