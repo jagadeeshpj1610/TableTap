7f2a5f1560ce406ed9496ce6497fa4e7d8d12f91
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-08 09:26:35 +0530

    #mod | PJ | some changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,32 @@
 import getMenu from "./api/menuApi";
 import { useState, useEffect } from "react";
-import FoodCard from './components/FoodCard'
+// import FoodCard from './components/FoodCard'
 
 
 function App() {
-  const [menuItems, setMenuItems] = useState([])
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getMenu()
-      setMenuItems(data)
-    };
-    fetchData()
-  }, [])
+  // const [menuItems, setMenuItems] = useState([])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await getMenu()
+  //     setMenuItems(data)
+  //   };
+  //   fetchData()
+  // }, [])
   return (
-    <div className="grid grid-cols-3 md:grid-cols-4 gap-3 p-3">
-      {menuItems.map((item) => (
-        <FoodCard
-          key={item._id}
-          image={item.imageUrl}
-          name={item.name}
-          description={item.description}
-          price={item.price}
-          isVeg={item.isVeg}
-          onAdd={() => console.log("added", item.name)}
-        />
-      ))}
-    </div>
+    // <div className="grid grid-cols-3 md:grid-cols-4 gap-3 p-3">
+    //   {menuItems.map((item) => (
+    //     <FoodCard
+    //       key={item._id}
+    //       image={item.imageUrl}
+    //       name={item.name}
+    //       description={item.description}
+    //       price={item.price}
+    //       isVeg={item.isVeg}
+    //       onAdd={() => console.log("added", item.name)}
+    //     />
+    //   ))}
+    // </div>
+    <></>
   )
 }
 export default App;
