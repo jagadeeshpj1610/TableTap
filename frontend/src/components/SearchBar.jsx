532d09e1e6eded7bdbe6d003b9a588d31db512ed
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-08 11:38:57 +0530

    #18 #C | PJ | added the styles for search component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
 
 
-const SearchBar = ({searchTerm, setSearchTerm}) => {
-    return(
-        <input type="text" placeholder="Search Dishes.." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+const SearchBar = ({ searchTerm, setSearchTerm }) => {
+    return (
+        <input type="text" placeholder="Search Dishes.." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full px-6 py-2 mt-2  rounded-full border border-neutral-200 text-sm text-neutral-900 focus:outline-none focus:ring-2 focus:ring-[#8B2635] focus:border-transparent"
+        />
     )
 }
 
