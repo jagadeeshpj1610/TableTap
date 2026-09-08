6c15387d077380b47a43eb73b2c9a5cb172c6724
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-08 09:44:26 +0530

    #18 #A | PJ | styled the header compoenent

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
 
 
 const Header = () => {
-    return(
-        <div>
+    return (
+        <div className="bg-[#FAF7F2] p-6 flex justify-between items-center">
             <div>
-                <h1>TableTap</h1>
-                <p>. Scan . Order . Relax</p>
+                <h1 className="font-bold text-2xl text-[#1A1A1A]">TableTap</h1>
+                <p className="text-sm text-[#767676]">Scan . Order . Relax</p>
+            </div>
+            <div>
+                <span className="bg-[#8B2635] text-white px-4 py-1 rounded-full text-sm font-medium">Table 5</span>
             </div>
         </div>
     )
