a085a48e670f05ed37af51b090acecb9ccc1935e
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-08 09:50:40 +0530

    #17 | PJ | food card code un-commented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,35 @@
 import getMenu from "./api/menuApi";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
-// import FoodCard from './components/FoodCard'
+import FoodCard from './components/FoodCard'
 
 
 function App() {
-  // const [menuItems, setMenuItems] = useState([])
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const data = await getMenu()
-  //     setMenuItems(data)
-  //   };
-  //   fetchData()
-  // }, [])
+  const [menuItems, setMenuItems] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getMenu()
+      setMenuItems(data)
+    };
+    fetchData()
+  }, [])
   return (
-    // <div className="grid grid-cols-3 md:grid-cols-4 gap-3 p-3">
-    //   {menuItems.map((item) => (
-    //     <FoodCard
-    //       key={item._id}
-    //       image={item.imageUrl}
-    //       name={item.name}
-    //       description={item.description}
-    //       price={item.price}
-    //       isVeg={item.isVeg}
-    //       onAdd={() => console.log("added", item.name)}
-    //     />
-    //   ))}
-    // </div>
     <>
-    <Header />
+      <Header />
+      <div className="grid grid-cols-3 md:grid-cols-4 gap-3 p-3">
+        {menuItems.map((item) => (
+          <FoodCard
+            key={item._id}
+            image={item.imageUrl}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            isVeg={item.isVeg}
+            onAdd={() => console.log("added", item.name)}
+          />
+        ))}
+      </div>
+
     </>
   )
 }
