ec6d1eb791c0fd1141f6f18560e3ed07b6248c6f
jagadeeshpj1610 <22022cm040@gmail.com>
2026-09-24 15:16:11 +0530

    #23 #B | PJ | overview component mount in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import CustomerApp from "./components/CustomerApp"
 import KitchenDashboard from "./components/KitechenDashboard";
 import AdminMenuManagement from "./components/AdminMenuManagement";
 import AdminOrderManagement from "./components/AdminOrderManagement";
+import AdminOverview from "./components/AdminOveriew";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/kitchen" element={<KitchenDashboard />} />
       <Route path="/admin" element={<AdminMenuManagement />} />
       <Route path="/admin/orders" element={<AdminOrderManagement />} />
+      <Route path="/admin/overview" element={<AdminOverview />} />
     </Routes>
   );
 }
